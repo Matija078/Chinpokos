55066ad67994f576b7b4d08d8b7bfd146d5b1ec2
Matija2003 <matijaj@viken.no>
2023-06-10 12:10:32 +0200

    Chinpokos

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import LoginPage from './components/LoginPage';
 
 ReactDOM.render(
   <Router>
-    <App />
+    <LoginPage />
   </Router>,
   document.getElementById('root')
 );
